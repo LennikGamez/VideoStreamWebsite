c620268d11d511bc6bf3354392031cd8a3d6cb70
Leander Doerk <leander@doerk.org>
2024-06-26 20:37:20 +0200

    playsinline needs to be active so only the custom controlelements show. embed replaced directly with svg o it doesnt lag on iphone when switching between icons

--- components/player.js
+++ components/player.js
@@ -190,8 +190,8 @@ class Player extends HTMLElement{
                     <div class="controls">
                         <div class="left-controls">
                             <button class="control-element play-btn">
-                            <embed class="play-icon" src="../assets/control-icons/play.svg">
-                            <embed class="pause-icon" src="../assets/control-icons/pause.svg">
+                                <svg class="play-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"/></svg>
+                                <svg class="pause-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M520-200v-560h240v560H520Zm-320 0v-560h240v560H200Zm400-80h80v-400h-80v400Zm-320 0h80v-400h-80v400Zm0-400v400-400Zm320 0v400-400Z"/></svg>
                             </button>
                         </div>
                         <div class="right-controls">
@@ -200,7 +200,7 @@ class Player extends HTMLElement{
                         </div>
                     </div>
                 </div>
-                <video autoplay>
+                <video autoplay playsinline>
                     <source id="source" src="">
                 </video>
             </div>
